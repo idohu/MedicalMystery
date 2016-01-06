447651ddf8a5e5c6c3ac5eb4372a5891fb70f352
idohu <idohubara@gmail.com>
2016-01-06 23:01:17 +0200

    Design Changes

--- MedicalMystery/Controllers/HomeController.cs
+++ MedicalMystery/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MedicalMystery.Models;
+using NQualtrics.Core;
 
 namespace MedicalMystery.Controllers
 {
@@ -10,6 +12,22 @@ namespace MedicalMystery.Controllers
     {
         public ActionResult Index()
         {
+            //Get Top Questionnaires
+            //Add Them To Model
+            List<QuestionnaireModel> l = new List<QuestionnaireModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                l.Add(new QuestionnaireModel
+                {
+                    QuestionaireEndDate = DateTime.Now.AddDays(2),
+                    QuestionnaireID = i.ToString(),
+                    QuestionnaireName = "Testing this "+i,
+                    QuestionnaireStartDate = DateTime.Now.AddDays(-Math.Pow(i,3)),
+                    Doctor = (i%2==0)?"Dr. Galit Almoznino DMD, M.Sc., MHA,": "Prof. Yuval Shahar, MD, PhD, FACMI",
+                    QuestionnaireDescription = "Just Do It!"
+                });
+            }
+            ViewBag.Questionnaires = l;
             return View();
         }
 
---
+++ MedicalMystery/Models/QuestionnaireModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalMystery.Models
+{
+    public class QuestionnaireModel
+    {
+        [Display(Name = "Questionnaire ID")]
+        public string QuestionnaireID;
+
+        [Display(Name = "Questionnaire ID")]
+        public string QuestionnaireName;
+
+        public string QuestionnaireDescription;
+
+        [Display(Name = "Questionnaire ID")]
+        public DateTime QuestionnaireStartDate;
+
+        [Display(Name = "Questionnaire ID")]
+        public DateTime QuestionaireEndDate;
+
+        public string Doctor;
+    }
+}
