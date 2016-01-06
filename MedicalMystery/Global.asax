3d12c92f7716ab0963d50cce43f1131a292af6af
idohu <idohubara@gmail.com>
2016-01-06 02:19:38 +0200

    First Upload

---
+++ MedicalMystery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MedicalMystery.MvcApplication" Language="C#" %>
---
+++ MedicalMystery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalMystery.Startup))]
+namespace MedicalMystery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
